ec0a820fb0bc814bb004c72301f32a7adf0051c6
KomorowskiKuba <60301726+KomorowskiKuba@users.noreply.github.com>
2024-11-07 20:51:04 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,13 +1,10 @@
 from flask import Flask,render_template
-import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
         return render_template('index.html', hostname='test', ip='123.123.123.123')
     except:
         return render_template('error.html')
