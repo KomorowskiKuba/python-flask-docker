96e15d97979247d360c8a7e250bae960b14f16ae
KomorowskiKuba <60301726+KomorowskiKuba@users.noreply.github.com>
2024-11-07 20:59:15 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
@@ -8,7 +10,6 @@ def index():
         return render_template('index.html', hostname='test', ip='123.123.123.123')
     except:
         return render_template('error.html')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=40204)
